8d6f53621a485ec752b02dd9e0101e5be0d896ba
Vladyslav D <donetsvlad444@gmail.com>
2024-12-02 17:19:54 +0200

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <>
+      <h2>Донець Владислав Віталійович</h2>
+      <p>Народився 3 лютого 2005 року в місті Ковель, Україна.</p>
+    </>
+  );
+}
---
+++ src/components/Image.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Image() {
+  return (
+    <a href="https://lviv.travel/" target="_blank" rel="noreferrer">
+      <img
+        src="https://aws-travel-guide.s3.eu-west-1.amazonaws.com/default_image_size/603fc39c503d9_03%20%D0%9B%D1%8C%D0%B2%D1%96%D0%B2.jpg"
+        alt="Місто Львів"
+      />
+    </a>
+  );
+}
